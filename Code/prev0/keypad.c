5a057caf55b27982e3ecd38abdee521a6feaf7ba
Shivesh Navin <shiveshnavin@gmail.com>
2017-07-26 12:31:10 +0530

    keypad start

---
+++ Code/prev0/keypad.c
@@ -0,0 +1,37 @@
+#include<pic.h>#include<string.h>
+
+
+/****KEYPAD  ******/
+
+int initkeypad();
+void clock();
+int scan();
+
+/****KEYPAD  ******/
+void main()
+{
+
+
+
+}
+
+/****KEYPAD  ******/
+
+int initkeypad()
+{
+
+}
+
+void clock()
+{
+
+
+}
+int scan()
+{
+
+
+}
+
+
+/****KEYPAD  ******/
